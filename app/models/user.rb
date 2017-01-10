f7390dcde5d6e9e00e2d5bbfe7e07e6610327ee2
linhai <linhai107@gmail.com>
2017-01-10 15:34:35 +0800

    join or quit group

--- app/models/user.rb
+++ app/models/user.rb
@@ -13,4 +13,13 @@ has_many :participated_groups, :through => :group_relationships, :source => :gro
      participated_groups.include?(group)
 
 end
+   def join!(group)
+     participated_groups << group
+   end
+
+   def quit!(group)
+     participated_groups.delete(group)
+   end
+
+   
 end
